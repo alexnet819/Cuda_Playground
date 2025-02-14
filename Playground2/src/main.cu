bb925fc4cca34445b3dde005db667cb0c85613bc
alexnet819 <alex@DESKTOP-N1NG6QF.>
2025-02-14 10:00:34 +0900

    fix

--- Playground2/src/main.cu
+++ Playground2/src/main.cu
@@ -10,13 +10,13 @@ __global__ void vectorAdd(int *a, int *b, int n) {
 	for(int j = 0; j < n; j++)
 		a[thread] = thread * b[j];
 }
+
 int main(int argc, char** argv) {
 
 	// GPUにデータを渡すようにvectorを作成
 	// NVIDIAがstd::vector相当のライブラリを用意してくれている
-	int *vec1[10], *vec2[20];
-	//thrust::host_vector<int> vec1(10);
-	//thrust::host_vector<int> vec2(20);
+	thrust::host_vector<int> vec1(10);
+	thrust::host_vector<int> vec2(20);
 	
 	// vectorに値を代入する
 	for (int i = 0; i < 20; i++) {
@@ -27,7 +27,7 @@ int main(int argc, char** argv) {
 
 	// vec1の中身を表示
 	std::cout << "Vector 1: ";
-	for (int i = 0; i < vec1.size(); i++)
+	for (int i = 0; i < 10; i++)
 	{
 		std::cout << vec1[i] << " ";
 	}
@@ -36,24 +36,20 @@ int main(int argc, char** argv) {
 
 	// GPUにvec1, vec2のデータを渡す
 	int *gpu_vec1, *gpu_vec2;
-	cudaMalloc((void**)&gpu_vec1, sizeof(vec1) * sizeof(int));
-	cudaMalloc((void**)&gpu_vec2, sizeof(vec2) * sizeof(int));
-	cudaMemcpy(gpu_vec1, vec1, sizeof(vec1) * sizeof(int), cudaMemcpyHostToDevice);
-	cudaMemcpy(gpu_vec2, vec2, sizeof(vec2) * sizeof(int), cudaMemcpyHostToDevice);
-
-
-	//thrust::device_vector<int> gpu_vec1 = vec1;
-	//thrust::device_vector<int> gpu_vec2 = vec2;
+	cudaMalloc((void**)&gpu_vec1, vec1.size() * sizeof(int));
+	cudaMalloc((void**)&gpu_vec2, vec2.size() * sizeof(int));
+	cudaMemcpy(gpu_vec1, vec1.data(), vec1.size() * sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(gpu_vec2, vec2.data(), vec2.size() * sizeof(int), cudaMemcpyHostToDevice);
 
 	// GPUで起動するthreadの数を決める
-	dim3 block(sizeof(vec1) / sizeof(int));
+	dim3 block(vec1.size());
 	dim3 grid(1);
 
 	// GPUでvectorAdd関数を実行
-	vectorAdd <<<grid, block>>>(gpu_vec1, gpu_vec2, sizeof(vec2) / sizeof(int);
+	vectorAdd <<<grid, block>>>(gpu_vec1, gpu_vec2, vec2.size());
 
 	// GPUで計算した結果をvec1にコピー
-	cudaMemcpy(vec1, gpu_vec1, sizeof(vec1) * sizeof(int), cudaMemcpyDeviceToHost);
+	cudaMemcpy(vec1.data(), gpu_vec1, vec1.size() * sizeof(int), cudaMemcpyDeviceToHost);
 	std::cout << "Vector 1: ";
 	for (int i = 0; i < vec1.size(); i++)
 	{
